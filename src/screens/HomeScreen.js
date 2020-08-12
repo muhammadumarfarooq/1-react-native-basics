ac37ab7037efd0e590a073d78c38a49f881fb4d9
Muhammad Umar Farooq <muhammadumarfarooq@users.noreply.github.com>
2020-08-12 19:31:41 +0500

    touchable opacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
   return (
@@ -9,6 +9,11 @@ const HomeScreen = () => {
         title="Go to Component Demo"
         onPress={() => console.log("Button pressed successfully!")}
       />
+      <TouchableOpacity onPress={() => console.log("Touchable Opacity clicked!")}>
+        <Text>Go to List Demo</Text>
+        <Text>Go to List Demo</Text>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
     </View>
   );
 };
