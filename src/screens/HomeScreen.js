d41757f27f0776e11eec935cc1e68639606787aa
Muhammad Umar Farooq <muhammadumarfarooq@users.noreply.github.com>
2020-08-15 11:22:48 +0500

    navigation to list component and destructuring properities

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = props => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>This is homescreen</Text>
       <Button
         title="Go to Component Demo"
-        onPress={() => props.navigation.navigate("Components")}
+        onPress={() => navigation.navigate("Components")}
       />
-      <TouchableOpacity onPress={() => console.log("Touchable Opacity clicked!")}>
-        <Text>Go to List Demo</Text>
-        {/*<Text>Go to List Demo</Text>*/}
-        {/*<Text>Go to List Demo</Text>*/}
-      </TouchableOpacity>
+      <Button
+        tittle={"Go to List Component"}
+        onPres={() => navigation.navigate("List")}
+      />
+      {/*<TouchableOpacity onPress={() => console.log("Touchable Opacity clicked!")}>*/}
+      {/*  <Text>Go to List Demo</Text>*/}
+      {/*  /!*<Text>Go to List Demo</Text>*!/*/}
+      {/*  /!*<Text>Go to List Demo</Text>*!/*/}
+      {/*</TouchableOpacity>*/}
     </View>
   );
 };
